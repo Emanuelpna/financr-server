1eee40d1bf01d07fec1b57efa15fab216e97ef5d
Emanuel Andrade <emanuelpna@yahoo.com.br>
2025-03-24 20:10:05 -0300

    Add Equatable to base entity class

--- Financr.GroceriesShoppingContext.Domain/Abstractions/Entity.cs
+++ Financr.GroceriesShoppingContext.Domain/Abstractions/Entity.cs
@@ -1,6 +1,11 @@
 namespace Financr.GroceriesShoppingContext.Domain.Abstractions;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Guid>
 {
     public Guid Id { get; protected init; } = Guid.NewGuid();
+    
+    public bool Equals(Guid other) => Id == other;
+
+    public override int GetHashCode() => Id.GetHashCode();
+
 }
